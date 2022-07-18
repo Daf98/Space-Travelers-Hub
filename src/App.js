dfaf0f8b79d3c02ba01699fa4fb5f85f9a3fda7c
Daf <dafne.azzolina@gmail.com>
2022-07-18 17:27:59 -0300

    Finish setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Missions from './components/Missions';
 import Navigation from './components/Navigation';
-import View from './components/View';
+import Profile from './components/Profile';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/view" element={<View />} />
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,23 +4,27 @@ import planet from '../images/planet.png';
 import './styles/Navigation.css';
 
 const Navigation = () => (
-  <nav className="navbar">
-    <div className="navTitle">
-      <img id="logo" src={planet} alt="planet-logo" />
-      <h1>Space Travelers Hub</h1>
-    </div>
-    <ul className="navLinks">
-      <li className="navLink">
-        <NavLink to="/rockets">Rockets</NavLink>
-      </li>
-      <li className="navLink">
-        <NavLink to="/missions">Missions</NavLink>
-      </li>
-      <li className="navLink">
-        <NavLink to="/profile">My Profile</NavLink>
-      </li>
-    </ul>
-  </nav>
+  <>
+    <nav className="navbar">
+      <div className="navTitle">
+        <img id="logo" src={planet} alt="planet-logo" />
+        <h1>Space Travelers Hub</h1>
+      </div>
+      <ul className="navLinks">
+        <li className="navLink">
+          <NavLink to="/">Rockets</NavLink>
+        </li>
+        <li className="navLink">
+          <NavLink to="/missions">Missions</NavLink>
+        </li>
+        <div id="separator" />
+        <li className="navLink">
+          <NavLink to="/profile">My Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+    <div id="horizontal" />
+  </>
 );
 
 export default Navigation;

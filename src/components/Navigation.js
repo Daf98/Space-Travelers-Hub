7631225634270802f02d950371309d572bf65953
Daf <dafne.azzolina@gmail.com>
2022-07-20 11:52:07 -0300

    Add style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => (
   <>
     <nav className="navbar">
       <div className="navTitle">
-        <img id="logo" src={planet} alt="planet-logo" />
+        <img id="logo" src={planet} alt="Logo" />
         <h1>Space Travelers Hub</h1>
       </div>
       <ul className="navLinks">

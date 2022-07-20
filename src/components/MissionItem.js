9b6ff5e4a221ead98e2aa8575fc62af6ae92f9b6
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-07-20 16:41:56 +0300

    Styling the buttons and change the buttons logic

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -1,62 +1,27 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { reserveMissions } from '../redux/missions/missions';
 import './styles/Missions.css';
 
 const MissionItem = (props) => {
   const {
     id, name, description, reserved,
   } = props;
-  const dispatch = useDispatch();
-
-  const handleReserved = (event) => {
-    const reserve = event.target.getAttribute('data-reserved');
-    const missionId = event.target.getAttribute('data-id');
-    dispatch(reserveMissions(missionId, reserve));
-  };
 
   return (
-    <tr key={id} className="missions">
+    <tr key={id}>
       <td>{name}</td>
       <td>{description}</td>
 
-      <td className="participation-status">
-        {reserved ? (
-          <button type="button" className="active-member">
-            Active Member
-          </button>
-        ) : (
-          <button type="button" className="not-member">
-            Not a Member
-          </button>
-        )}
+      <td>
+        <span className={reserved ? 'activeMember' : 'notMember'}>
+          {reserved ? 'Active Member' : 'Not A Member'}
+        </span>
       </td>
 
       <td>
-        {reserved ? (
-          <button
-            type="button"
-            className="leave-mission"
-            id={id}
-            data_id={id}
-            data-reserved={0}
-            onClick={handleReserved}
-          >
-            Leave Mission
-          </button>
-        ) : (
-          <button
-            type="button"
-            className="join-mission"
-            id={id}
-            data_id={id}
-            data-reserved={1}
-            onClick={handleReserved}
-          >
-            Join Mission
-          </button>
-        )}
+        <button type="button" className={reserved ? 'leaveMission' : ''}>
+          {reserved ? 'Leave Mission' : 'Join Mission'}
+        </button>
       </td>
     </tr>
   );
